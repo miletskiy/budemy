5c97834531940b7dcbf138daa2936e97987c1398
miletskiy <s.miletskiy@gmail.com>
2017-05-15 14:31:31 +0300

    Link nav buttons to sections. Optimize images. Smoothe scrolling

--- js/main.js
+++ js/main.js
@@ -15,6 +15,15 @@ $(document).ready(function(){
             $('.navbar').removeClass('navbar-fixed-top');
         }
     })
-
 });
+$('a[href^="#"]').on('click', function(e){
+    e.preventDefault();
 
+    var hash = this.hash;
+ 
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 400, function(){
+        window.location.hash = hash;
+    });
+})
