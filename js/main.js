aead47807643f7e7b7a21c70b37e1de8989c6457
miletskiy <s.miletskiy@gmail.com>
2017-05-15 13:49:15 +0300

    Fix to top navbar when scroll to the navbar menu

--- js/main.js
+++ js/main.js
@@ -2,4 +2,19 @@
 // $('a').click(function () {
 //   alert("Hello, World!");
 // });
+$(document).ready(function(){
+    var screenHeight = 720;
+    // console.log($(window).height());
+    // console.log($(document).height());
+    $(window).scroll(function(){
+
+        // console.log($(window).scrollTop());
+        if($(window).scrollTop() > screenHeight){
+            $('.navbar').addClass('navbar-fixed-top');
+        } else{
+            $('.navbar').removeClass('navbar-fixed-top');
+        }
+    })
+
+});
 
